e30a9fcbc53cead76aba70b233d85045ec5efff3
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-09-06 20:40:01 -0300

    tests: creates a point at a certain space position

--- tests/unit/tuples/Point.cpp
+++ tests/unit/tuples/Point.cpp
@@ -10,6 +10,16 @@ MU_TEST(test_should_create_a_point_at_space_origin) {
     mu_assert_double_eq(1, p.w);
 }
 
+MU_TEST(test_should_create_a_point_at_certain_space_position) {
+    Point p(3, 4, 5);
+
+    mu_assert_double_eq(3, p.x);
+    mu_assert_double_eq(4, p.y);
+    mu_assert_double_eq(5, p.z);
+    mu_assert_double_eq(1, p.w);
+}
+
 MU_TEST_SUITE(test_point_suite) {
     MU_RUN_TEST(test_should_create_a_point_at_space_origin);
+    MU_RUN_TEST(test_should_create_a_point_at_certain_space_position);
 }
