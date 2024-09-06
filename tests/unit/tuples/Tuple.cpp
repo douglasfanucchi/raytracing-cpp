d7d8c255c43dfe722eae90881a12afd12e294f63
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-09-06 20:24:10 -0300

    tests: test creates tuple with values different than zeros

--- tests/unit/tuples/Tuple.cpp
+++ tests/unit/tuples/Tuple.cpp
@@ -11,6 +11,16 @@ MU_TEST(test_should_create_a_tuple) {
     mu_assert_double_eq(expected, tuple.w);
 }
 
+MU_TEST(test_should_create_a_tuple_with_values_different_than_0) {
+    Tuple tuple(3, 10, 11, 7);
+
+    mu_assert_double_eq(3, tuple.x);
+    mu_assert_double_eq(10, tuple.y);
+    mu_assert_double_eq(11, tuple.z);
+    mu_assert_double_eq(7, tuple.w);
+}
+
 MU_TEST_SUITE(test_tuple_suite) {
     MU_RUN_TEST(test_should_create_a_tuple);
+    MU_RUN_TEST(test_should_create_a_tuple_with_values_different_than_0);
 }
