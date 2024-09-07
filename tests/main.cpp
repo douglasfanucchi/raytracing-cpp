543301c6bbc9209166dc005ab7def0038f89a0dd
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-09-06 21:00:19 -0300

    tests: creates a non null vector

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,10 +1,12 @@
 #include <minunit.h>
 #include "unit/tuples/Tuple.cpp"
 #include "unit/tuples/Point.cpp"
+#include "unit/tuples/Vector.cpp"
 
 int main(int argc, char *argv[]) {
 	MU_RUN_SUITE(test_tuple_suite);
 	MU_RUN_SUITE(test_point_suite);
+	MU_RUN_SUITE(test_vector_suite);
 	MU_REPORT();
 	return 0;
 }
--- tests/unit/tuples/Vector.cpp
+++ tests/unit/tuples/Vector.cpp
@@ -11,6 +11,16 @@ MU_TEST(test_should_create_null_vector) {
     mu_assert_double_eq(expected, v.w);
 }
 
+MU_TEST(test_should_create_a_non_null_vector) {
+    Vector v(3.0, 4.0, 5.0);
+
+    mu_assert_double_eq(3, v.x);
+    mu_assert_double_eq(4, v.y);
+    mu_assert_double_eq(5, v.z);
+    mu_assert_double_eq(0, v.w);
+}
+
 MU_TEST_SUITE(test_vector_suite) {
     MU_RUN_TEST(test_should_create_null_vector);
+    MU_RUN_TEST(test_should_create_a_non_null_vector);
 }
