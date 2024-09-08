7f095ac1100ae0a130bbbdf2d464994dccdaa8ab
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-09-08 04:24:16 -0300

    tests: compares two tuples as equal

--- tests/unit/tuples/Tuple.cpp
+++ tests/unit/tuples/Tuple.cpp
@@ -1,5 +1,6 @@
 #include <minunit.h>
 #include <Tuple.hpp>
+#include <cmath>
 
 MU_TEST(test_should_create_a_tuple) {
     Tuple tuple;
@@ -20,7 +21,15 @@ MU_TEST(test_should_create_a_tuple_with_values_different_than_0) {
     mu_assert_double_eq(7, tuple.w);
 }
 
+MU_TEST(test_should_compare_two_tuple_as_equal) {
+    Tuple tuple(M_PI, M_SQRT2, 1, 7);
+    Tuple tuple2(M_PI, M_SQRT2, 1, 7);
+
+    mu_check(tuple == tuple2);
+}
+
 MU_TEST_SUITE(test_tuple_suite) {
     MU_RUN_TEST(test_should_create_a_tuple);
     MU_RUN_TEST(test_should_create_a_tuple_with_values_different_than_0);
+    MU_RUN_TEST(test_should_compare_two_tuple_as_equal);
 }
