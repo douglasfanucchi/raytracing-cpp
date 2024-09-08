efe78be390cb5ab8d749afde6c295048737d50c8
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-09-08 04:40:21 -0300

    feat: implements Tuple::operator==(const Tuple&)

--- src/tuples/Tuple.cpp
+++ src/tuples/Tuple.cpp
@@ -3,3 +3,12 @@
 Tuple::Tuple(void) : x(0), y(0), z(0), w(0) {}
 
 Tuple::Tuple(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {}
+
+bool Tuple::operator==(const Tuple &tuple) const {
+    float epsilon = 1E-12;
+
+    return fabs(this->x - tuple.x) < epsilon &&
+           fabs(this->y - tuple.y) < epsilon &&
+           fabs(this->z - tuple.z) < epsilon &&
+           fabs(this->w - tuple.w) < epsilon;
+}
